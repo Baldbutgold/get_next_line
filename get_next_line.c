a73a0464d670697621d2be716dbb88825a22d708
brr <abdouplayz2003@gmail.com>
2024-12-10 23:24:57 +0100

    working on memory leaks

--- get_next_line.c
+++ get_next_line.c
@@ -8,7 +8,7 @@ char	*get_next_line(int fd)
 	ssize_t		read_pos;
 	int		newline_pos;
 
-	return_line = NULL;;
+	return_line = NULL;
 	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
 	buffer = malloc(BUFFER_SIZE + 1);
@@ -23,16 +23,26 @@ char	*get_next_line(int fd)
 		if (buffer[0] == '\0')
 			return (NULL);
 		line = ft_strjoin(line, buffer);
+		if (!line)
+			return (free(line), free(buffer), NULL);
 	}
-	if ((newline_pos = ft_strchr(line, '\n')) >= 0 || read_pos == 0)
+	if (ft_strchr(line, '\n') >= 0 || read_pos == 0)
 	{
+		newline_pos = ft_strchr(line, '\n');
 		if (read_pos == 0)
 			return_line = ft_substr(line, 0, ft_strlen(line));
 		else
 		{
-		return_line = ft_substr(line, 0, newline_pos + 1);
-		line = line + newline_pos + 1;
+			return_line = ft_substr(line, 0, newline_pos + 1);
+			if (!return_line)
+				return (free(return_line), free(line), free(buffer), NULL);
+			//line = ft_substr(line + newline_pos + 1, ft_strlen(line));
+			line = line + newline_pos + 1;
 		}
 	}
+	free(buffer);
 	return (return_line);
 }
+// free when in problem
+// test if malloc a string twice does it free correctly
+// free if read gives -1
